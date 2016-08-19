a90b6b8b1d35112ba1afbebb6465a54d5f9bba33
skuhinek@gmail.com <skuhinek@gmail.com>
2016-08-19 12:30:54 +0200

    formatiranje koda

--- src/img_2.rs
+++ src/img_2.rs
@@ -21,8 +21,7 @@ pub enum Resize {
     Neather,
 }
 
-pub struct ImageSize
-{
+pub struct ImageSize {
     pub width: u32,
     pub height: u32,
     path: PathBuf,
@@ -31,10 +30,8 @@ pub struct ImageSize
     image_format: ImageFormat,
 }
 
-impl ImageSize 
-{
-    pub fn new() -> ImageSize
-    {
+impl ImageSize {
+    pub fn new() -> ImageSize {
         ImageSize{
             width: constants::OUTPUT_WIDTH, 
             height: constants::OUTPUT_HEIGHT,
@@ -45,8 +42,7 @@ impl ImageSize
         }
     }
 
-    pub fn from(path: PathBuf, arguments: &Arguments) -> ImageSize
-    {                    
+    pub fn from(path: PathBuf, arguments: &Arguments) -> ImageSize {                    
         ImageSize{
             width: arguments.width, 
             height: arguments.height,
@@ -57,8 +53,7 @@ impl ImageSize
         }
     }
     
-    fn resize_to_width(&mut self, new_width: u32) -> bool//,  rsz_type: &Resize)
-    {
+    fn resize_to_width(&mut self, new_width: u32) -> bool {
        let calc_new_size;// = false;
         match self.resize {
             Resize::Increase => calc_new_size = self.width < new_width,                
@@ -67,16 +62,14 @@ impl ImageSize
             Resize::Neather => calc_new_size = false,
         }
 
-        if calc_new_size
-        {
+        if calc_new_size{
             self.height = new_width * self.height / self.width;
             self.width = new_width;
         }
         calc_new_size
     }
     
-    fn resize_to_height(&mut self, new_height: u32) -> bool//, rsz_type: &Resize)
-    {
+    fn resize_to_height(&mut self, new_height: u32) -> bool {
         let calc_new_size;// = false;
 
         match self.resize {
@@ -93,24 +86,19 @@ impl ImageSize
         }
         calc_new_size
     }
-
-    //TODO: ako ne doće do promjene veličeine i file je isti samo promijeniti naziv datotoeke
-    pub fn resize_image(&mut self)
-    {
-        if !self.is_supported()
-        {
+    
+    pub fn resize_image(&mut self) {
+        if !self.is_supported() {
             println!("File is not supported:\n{:?}\n", self.path);
             return;
         }
         
-        if let Some(img) = image::open(&self.path).ok()
-        {        
+        if let Some(img) = image::open(&self.path).ok() {        
             let mut width_from_args = self.width;
             let mut height_from_args = self.height;
             let mut is_size_changed = true;
             
-            if width_from_args == 0 && height_from_args == 0
-            {
+            if width_from_args == 0 && height_from_args == 0 {
                 width_from_args = constants::OUTPUT_WIDTH;
                 height_from_args = constants::OUTPUT_HEIGHT;
             }
@@ -119,87 +107,63 @@ impl ImageSize
             self.width = img.dimensions().0;
             self.height = img.dimensions().1;        
 
-            if width_from_args != 0 && height_from_args != 0
-            {
-                if self.width > self.height
-                {                    
+            if width_from_args != 0 && height_from_args != 0 {
+                if self.width > self.height {                    
                    is_size_changed = self.resize_to_width(width_from_args);
-                }
-                else
-                {
+                } else {
                    is_size_changed = self.resize_to_height(height_from_args);
                 }
-            }        
-            else if width_from_args != 0
-            {            
+            } else if width_from_args != 0 {            
                is_size_changed = self.resize_to_width(width_from_args);
-            }
-            else if height_from_args != 0
-            {
+            } else if height_from_args != 0 {
                is_size_changed = self.resize_to_height(height_from_args); 
-            }
-            else
-            {
+            } else {
                 unreachable!("img_2::ImageSize::resize_image");
-                //panic!("Should not happened in img_2::ImageSize::resize_image");
             }
 
             let out_path_buf = out_file_name(&self.path, self.image_format);
-            let out_path =  Path::new(out_path_buf.to_str().unwrap());//Path = Path::new(out_file_name(&path).to_str().unwrap());
-
+            let out_path =  Path::new(out_path_buf.to_str().unwrap());
 
             // kopirat će i preimenovati file ukoliko se velićina nije promjenila i extenzija je ostala ista
-            if !is_size_changed && image_format_to_string(self.image_format) == self.file_extension()
-            {  
+            if !is_size_changed && image_format_to_string(self.image_format) == self.file_extension() {  
                 let _ = fs::copy(self.path.clone(), out_path);
-            }
-            else
-            {
+            } else {
                 let ref mut fout = File::create(&out_path).unwrap();
                 let resized_image = img.resize_exact(self.width, self.height, self.filter);
                 let _ = resized_image.save(fout, self.image_format).unwrap();
                 println!("Image {:?}\nresized to: {}x{}\n", &self.path.file_name().unwrap(), self.width, self.height);
             }
-
         } else {
             println!("error opening image {:?}", &self.path);
         }
     }
 
-    pub fn file_extension(&self) -> String
-    {
+    pub fn file_extension(&self) -> String {
         let mut extension = String::new();
-        if let Some(ext_osstr) = self.path.extension()
-        {
+        if let Some(ext_osstr) = self.path.extension() {
             extension = string_to_lower(ext_osstr.to_str().unwrap());
         }
         extension
     }
 
-    pub fn is_supported(&self) -> bool
-    {
+    pub fn is_supported(&self) -> bool {
         let ext: &str = &self.file_extension();
-        let suported_files = constants::SUPPORTED_FILES;//["jpg","png","jpeg", "gif", "bmp", "tiff"]; // ovo je definirano kao konstanta
-        for i in 0..suported_files.len()
-        {
-            if ext == suported_files[i]
-            {
+        let suported_files = constants::SUPPORTED_FILES;
+        for i in 0..suported_files.len() {
+            if ext == suported_files[i] {
                 return true;
             }
-        }
-        
+        }        
         false
     }    
 }
 
-pub fn resize_images(mut args: Arguments)
-{
+pub fn resize_images(mut args: Arguments) {
     let mut threads = Vec::new();
     let mut i: usize = 0;
     let paths = args.paths();
     args.paths_clear();
-    for path in paths
-    {           
+    for path in paths {           
         let arg = args.clone();
         threads.push(thread::spawn(move || { 
             let mut is = ImageSize::from(path, &arg);
@@ -208,26 +172,21 @@ pub fn resize_images(mut args: Arguments)
         
         // maximalan broj slika koje će se odjednom učitati i obraditi
         i += 1;
-        if i % args.max_parallel_img == 0
-        {
-            for thread in threads
-            {
+        if i % args.max_parallel_img == 0 {
+            for thread in threads {
                 thread.join().unwrap();
             }
             threads = Vec::new();
         } 
     }
 
-    for thread in threads 
-    {
+    for thread in threads {
         thread.join().unwrap();
     }
 
 }
 
-fn out_file_name(path: &Path, image_format: ImageFormat) -> PathBuf
-{
-    //let suffiks = "-m";
+fn out_file_name(path: &Path, image_format: ImageFormat) -> PathBuf {
     let name: &str = path.file_stem().unwrap().to_str().unwrap();// + suffiks + ".jpg";
 
     let out_name = String::from(name) + constants::FILE_NAME_SUFFIX + "." + &image_format_to_string(image_format);
@@ -237,8 +196,7 @@ fn out_file_name(path: &Path, image_format: ImageFormat) -> PathBuf
     path_buf    
 }
 
-fn image_format_to_string(image_format: ImageFormat) -> String
-{
+fn image_format_to_string(image_format: ImageFormat) -> String {
     let extension: String;
     match image_format {
         ImageFormat::JPEG => extension = "jpg".to_string(),
@@ -248,7 +206,6 @@ fn image_format_to_string(image_format: ImageFormat) -> String
     extension
 }
 
-fn string_to_lower(s: &str) -> String
-{
+fn string_to_lower(s: &str) -> String {
     s.to_string().to_lowercase()
 }
